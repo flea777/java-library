8965c81509dc6be37bf2a3371870f2b92089de61
yuricsg <yuriicardo004@gmail.com>
2025-04-02 14:28:22 -0300

    first commit

---
+++ src/main/java/com/java_library/cesar/CesarProjectApplication.java
@@ -0,0 +1,13 @@
+package com.java_library.cesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesarProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_library/cesar/CesarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_library.cesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
